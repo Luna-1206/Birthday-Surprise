b0a74d82ab3368c5c85dba49909d53c78b1b8b53
Lee-stud <jb2.laluna2026@nrgitii.edu.ph>
2026-07-14 13:34:44 +0800

    Update TimelineScreen.jsx

--- src/components/timeline/TimelineScreen.jsx
+++ src/components/timeline/TimelineScreen.jsx
@@ -99,6 +99,15 @@ const subtitleOpacity = useTransform(
 
     );
 
+    const backgroundY = useTransform(
+
+        scrollYProgress,
+
+        [0,1],
+
+        [80, -80]
+    );
+
     return (
 
         <section
