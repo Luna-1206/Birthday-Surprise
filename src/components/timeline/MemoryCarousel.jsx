dba0b1b962770f253516aa17f69b0a562cb5b382
Lee-stud <jb2.laluna2026@nrgitii.edu.ph>
2026-07-14 23:55:28 +0800

    UPD

--- src/components/timeline/MemoryCarousel.jsx
+++ src/components/timeline/MemoryCarousel.jsx
@@ -1,21 +1,115 @@
 import { useState } from "react";
 
-import { motion, AnimatePresence } from "framer-motion";
-
-import Polaroid from "./Polaroid";
-
 import "../../styles/timeline/MemoryCarousel.css";
-
 
 function MemoryCarousel({ memories }) {
 
+    const [current, setCurrent] = useState(0);
+
+    const previous =
+        (current - 1 + memories.length) % memories.length;
+
+    const next =
+        (current + 1) % memories.length;
+
     return (
 
-        <div>
+        <div className="memoryCarousel">
 
-            <h2>Carousel Placeholder</h2>
+            <div className="carouselWrapper">
 
-            <p>Total Memories: {memories.length}</p>
+                {/* Previous Card */}
+                <div className="memoryCard sideCard">
+
+                    <img
+                        src={memories[previous].image}
+                        alt={memories[previous].title}
+                    />
+
+                </div>
+
+
+                {/* Left Arrow */}
+
+                <button className="left">
+
+                    ❮
+
+                </button>
+
+
+                {/* Current Card */}
+
+                <div className="memoryCard currentCard"
+                
+                    className="memoryCard currentCard"
+                    onClick={() => {
+
+                        console.log("Expand Card");
+                        
+                    }}
+
+                >    
+
+                    <h2 className="memoryTitle">
+                        
+                        {memories[current].title}
+                        
+                    </h2>
+
+                    <div className="memoryDividerCarousel"></div>           
+
+                    <img
+                        src={memories[current].image}
+                        alt={memories[current].title}
+                    />
+
+                    <p className="memoryDate">
+
+                        {memories[current].date}
+
+                    </p>
+
+                    <div className="photoOverlay">
+
+                        <span>
+
+                            Reveal this memory
+
+                        </span>
+
+                        <span className="expandIcon">
+
+                            ⌄
+
+                        </span>
+                        
+                    </div>
+
+                </div>
+
+
+                {/* Right Arrow */}
+
+                <button className="right">
+
+                    ❯ 
+
+                </button>
+
+
+                {/* Next Card */}
+
+                <div className="memoryCard sideCard">
+
+                    <img
+                        src={memories[next].image}
+                        alt={memories[next].title}
+                    />
+
+                </div>
+
+            </div>
 
         </div>
 
